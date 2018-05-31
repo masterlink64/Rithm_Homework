b81f35972ba3fdc4a0b8309407e24e00bd6d9c2a
masterlink64 <steve.cao7@gmail.com>
2018-05-31 16:59:47 -0700

    finished part 1 exercises

--- ES6-jQuery/ES6-HW.js
+++ ES6-jQuery/ES6-HW.js
@@ -3,14 +3,14 @@ var name = 'Michael';
 var favoriteColor = 'purple';
 var city = 'San Francisco';
 
-console.log(
-  name +
-    ' lives in ' +
-    city +
-    ' and his favorite color is ' +
-    favoriteColor +
-    '.'
-);
+// console.log(
+//   name +
+//     ' lives in ' +
+//     city +
+//     ' and his favorite color is ' +
+//     favoriteColor +
+//     '.'
+// );
 
 // ES6 template literal
 var name = 'Michael';
@@ -18,12 +18,12 @@ var favoriteColor = 'purple';
 var city = 'San Francisco';
 
 console.log(
-  '${name} lives in ${city} and his favorite color is ${favoriteColor}'
+  `${name} lives in ${city} and his favorite color is ${favoriteColor}`
 );
 
 // ES5 global constants
-var PI = 3.14;
-PI = 42; // stop me from doing this!
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
 
 // ES6 constants
 const PI = 3.14;
@@ -31,43 +31,45 @@ const PI = 3.14;
 // ES5 assigning Variables to Object properties
 var obj = {
     numbers: {
-      a: 1,
-      b: 2
+      x: 1,
+      y: 2
     }
   };
   
-  var a = obj.numbers.a;
-  var b = obj.numbers.b;
+//   var a = obj.numbers.a;
+//   var b = obj.numbers.b;
 
 // ES6 Object Destructuring
 
-
+let {numbers} = obj;
+// console.log(numbers);
+let {x, y} = numbers;
+console.log(x, y);
 // ES5 Array Swap
 
 var arr = [1, 2];
-var temp = arr[0];
-arr[0] = arr[1];
-arr[1] = temp;
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
 
 // ES6 One-Line Array Swap with Destructuring
-
-
+[arr[0], arr[1]] = [arr[1], arr[0]];  
+console.log(arr);
 // ES5 Map Callback
 
-function double(arr) {
-    return arr.map(function(val) {
-      return val * 2;
-    });
-  }
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
 
 //ES6 Map Callback Shorthand
-
+let nums = [1,2,3,4];
 function double(arr) {
-    return arr.map((val, idx) => {
-        return val * 2;
-    });
+    return arr.map(val => val * 2);
 }
 
+console.log(double(nums));
 // ES5 Default Arguments
 function add(a, b) {
     if (a === 0) {
@@ -88,21 +90,26 @@ function add(a, b) {
 function add(a = 10, b = 10) {
   return a + b
 }
-
+console.log(add());
 // ES5 Function that takes a variable number of arguments
 
-function sumMany() {
-    var nums = Array.prototype.slice.call(arguments);
-    return nums.reduce(function(a, b) {
-      return a + b;
-    }, 0);
-}
+// function sumMany() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.reduce(function(a, b) {
+//       return a + b;
+//     }, 0);
+// }
 
 // ES6 Function that takes a variable number of arguments
 
-function sumMany(...theArgs) {
-    var nums = Array.prototype.slice.call(theArgs);
-    return nums.reduce(function(a, b) {
-      return a + b;
-    }, 0);
+function sumMany(...funES) {
+  return funES.reduce((acc, curr) => {
+      return acc + curr
+  }, 0);
 }
+// or
+function sumMany(...funES) {
+    return funES.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sumMany(4, 5, 6));
