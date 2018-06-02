28b37cc4b5fbea85ec990c34dae5205cf4c20adc
masterlink64 <steve.cao7@gmail.com>
2018-06-02 10:33:37 -0700

    moved things around

---
+++ Week1/ES6-jQuery/guess-the-password/app.js
@@ -0,0 +1,123 @@
+/* document.addEventListener('DOMContentLoaded', */
+$(document).ready(function(){
+  // change const and lets for ES6
+  const wordCount = 10;
+  let guessCount = 4;
+  let password = '';
+
+  //let start = document.getElementById('start');
+  // start.addEventListener('click', /*function()*/ () => {
+  //   toggleClasses(document.getElementById('start-screen'), 'hide', 'show');
+  //   toggleClasses(document.getElementById('game-screen'), 'hide', 'show');
+  //   startGame();
+  // });
+  
+  // j Query
+  var start = $('#start');
+  start.on('click', function() {
+    $('#start-screen').toggleClass('hide show');
+    $('#game-screen').toggleClass('hide show');
+    startGame();
+  });
+
+  //DO NOT NEED a toggle classes function for jQuery
+  // function toggleClasses(element, ...classNames) {
+  //   // changing to ES6
+  //   // for (var i = 1; i < arguments.length; i++) {
+  //   //   element.classList.toggle(arguments[i]);
+  //   // }
+  //   return classNames.forEach((el, idx) => {
+  //     return element.classList.toggle(el);
+  //   })
+  // }
+
+  function startGame() {
+    // get random words and append them to the DOM
+    let wordList = document.getElementById('word-list');
+    // var wordList = $('#word-list');
+    // 'words' variable is from words.js
+    var randomWords = getRandomValues(words, wordCount); // eslint-disable-line no-undef
+    // randomWords.forEach(function(word) {
+    //   let li = document.createElement('li');
+    //   li.innerText = word;
+    //   wordList.appendChild(li);
+    // });
+    //ES6
+    randomWords.forEach((word) => {
+      let li = document.createElement('li');
+      //var li = $('<li>').text(`${word}`);
+      li.innerText = word;
+      wordList.appendChild(li);
+      wordList
+    });
+
+    // set a secret password and the guess count display
+    password = getRandomValues(randomWords, 1)[0];
+    setGuessCount(guessCount);
+
+    // add update listener for clicking on a word
+    wordList.addEventListener('click', updateGame);
+  }
+
+  function getRandomValues(array, numberOfVals) {
+    return shuffle(array).slice(0, numberOfVals);
+  }
+
+  function shuffle(array) {
+    // let arrayCopy = array.slice();
+    // ES6
+    let arrayCopy = [...array];
+    for (let idx1 = arrayCopy.length - 1; idx1 > 0; idx1--) {
+      // generate a random index between 0 and idx1 (inclusive)
+      let idx2 = Math.floor(Math.random() * (idx1 + 1));
+
+      // swap elements at idx1 and idx2
+      // ES6
+      // var temp = arrayCopy[idx1];
+      // arrayCopy[idx1] = arrayCopy[idx2];
+      // arrayCopy[idx2] = temp;
+      [arrayCopy[idx2],arrayCopy[idx1]] = [arrayCopy[idx1], arrayCopy[idx2]];
+    }
+    return arrayCopy;
+  }
+
+  function setGuessCount(newCount) {
+    guessCount = newCount;
+    document.getElementById('guesses-remaining').innerText =
+      `Guesses remaining: ${guessCount}.`;
+  }
+
+  function updateGame(e) {
+    if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
+      // grab guessed word, check it against password, update view
+      let guess = e.target.innerText;
+      let similarityScore = compareWords(guess, password);
+      e.target.classList.add('disabled');
+      e.target.innerText = `${guess} --> Matching Letters: ${similarityScore}`;
+      setGuessCount(guessCount - 1);
+
+      // check whether the game is over
+      if (similarityScore === password.length) {
+        // toggleClasses was an old function for vanilla JS
+        // NEED TO FIX
+        toggleClasses(document.getElementById('winner'), 'hide', 'show');
+        this.removeEventListener('click', updateGame);
+      } else if (guessCount === 0) {
+        toggleClasses(document.getElementById('loser'), 'hide', 'show');
+        this.removeEventListener('click', updateGame);
+      }
+    }
+  }
+
+  function compareWords(word1, word2) {
+    if (word1.length !== word2.length) {
+      throw 'Words must have the same length';
+    }
+    let count = 0;
+    // can I convert this to es6?
+    for (let i = 0; i < word1.length; i++) {
+      if (word1[i] === word2[i]) count++;
+    }
+    return count;
+  }
+});
