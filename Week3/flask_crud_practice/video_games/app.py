0de2f164b87d4ce7843f8fa395e9a0e5ccbf8d89
masterlink64 <steve.cao7@gmail.com>
2018-06-16 21:05:20 -0700

    added route to put games on display

--- Week3/flask_crud_practice/video_games/app.py
+++ Week3/flask_crud_practice/video_games/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 class Game:
     count = 1
 
-    def __init__(self, name, system, rating, id):
+    def __init__(self, name, system, rating):
         self.name = name
         self.system = system
         self.rating = rating
@@ -14,6 +14,19 @@ class Game:
         Game.count += 1
 
 
+zelda = Game('Zelda: Breath of the Wild', 'Switch', 97)
+horizon = Game('Horizon Zero Dawn', 'PS4', 89)
+mario = Game('Super Mario Odyssey', 'Switch', 97)
+injustice = Game('Injustice 2', 'Xbox One', 89)
+
+games = [zelda, horizon, mario, injustice]
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/games')
+def index():
+    return render_template('index.html', games=games)
